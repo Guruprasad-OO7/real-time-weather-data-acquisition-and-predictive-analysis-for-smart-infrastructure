795c0ae4698494b7ee41b484de0edc60b4d2df3c
Guruprasad-OO7 <guruprasad67.hbr@gmail.com>
2025-07-01 13:58:35 +0530

    C

---
+++ Gpio.c
@@ -0,0 +1,25 @@
+#include "gpio.h"
+void gpio_init(void) 
+{
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+    gpio_pin_mode(GPIOA, 8, INPUT_FLOATING);
+    gpio_pin_mode(GPIOA, 11, INPUT_FLOATING);
+}
+void gpio_pin_mode(GPIO_TypeDef *port, uint8_t pin, uint8_t mode) 
+{
+    uint32_t shift = (pin % 8) * 4;
+    volatile uint32_t *cr = (pin < 8) ? &port->CRL : &port->CRR;
+
+    if (pin < 8) {
+        *cr &= ~(0xF << shift);
+        *cr |= (mode << shift);
+    } else {
+        cr = &port->CRH;
+        *cr &= ~(0xF << shift);
+        *cr |= (mode << shift);
+    }
+}
+uint8_t read_gpio_pin(GPIO_TypeDef *port, uint8_t pin) 
+{
+    return (port->IDR & (1 << pin)) ? 1 : 0;
+}
